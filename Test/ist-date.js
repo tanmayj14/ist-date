c5cd69bba4f2c3a6d5997335625bd84be139558f
Tanmay <tanmay.jha@moreyeahs.in>
2023-01-17 10:42:04 +0530

    t

--- Test/ist-date.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.istDate = void 0;
-const istDate = function () {
-    let datev = new Date().getTime() + 5.5 * 3600 * 1000;
-    return new Date(datev);
-};
-exports.istDate = istDate;
